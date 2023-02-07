5da161bfdf93168c36201e865f3e061b8eddfe54
Paul McLanahan <pmac@mozilla.com>
2023-02-07 15:50:57 -0500

    Add page_url context to template render

--- build.py
+++ build.py
@@ -18,12 +18,24 @@ def get_output_path(page_path):
     return OUTPUT_PATH.joinpath(out_path)
 
 
+def get_page_url(page_path):
+    """Returns the URL for the template"""
+    if page_path.name == "index.html":
+        if str(page_path) == "index.html":
+            # root page
+            return "/"
+
+        return f"/{page_path.parent}/"
+
+    return f"/{page_path.stem}/"
+
+    
 def render_page(page_path):
     """Renders a page template and writes it to an output file"""
     page = env.get_template(str(page_path))
     out_path = get_output_path(page_path)
     out_path.parent.mkdir(exist_ok=True, parents=True)
-    out_path.write_text(page.render())
+    out_path.write_text(page.render(page_url=get_page_url(page_path)))
     print(f"- {out_path}")
 
 
